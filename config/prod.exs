797e7b175cd49222c79afcd461c394e49b2350f9
evettetelyas <etelyas@gmail.com>
2020-01-14 22:51:48 -0700

    config prod for deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :koroibos_ex, KoroibosExWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+url: [scheme: "https", host: "koroibos-ex", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
+cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
